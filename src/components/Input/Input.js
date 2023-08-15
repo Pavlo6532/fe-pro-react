4234cd7bcf0ff974865f1f24be1378e43c7a85f1
Pavlo6532 <lpn3005@gmail.com>
2023-08-15 12:58:26 +0300

    logic login page

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Input.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-const Input = ({ type, placeholder }) => {
+const Input = ({ type, placeholder, value, onChange, errorMessage }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const toggleShowPassword = () => {
@@ -10,13 +10,24 @@ const Input = ({ type, placeholder }) => {
   };
 
   return (
-    <div className="input-container">
-      <input type={showPassword ? "text" : type} placeholder={placeholder} />
+    <div
+      className={`input-container ${
+        errorMessage ? "error-form-group" : "input-form-group"
+      }`}
+    >
+      <input
+        type={showPassword ? "text" : type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="input-field"
+      />
       {type === "password" && (
         <div className="eye-icon" onClick={toggleShowPassword}>
           {showPassword ? <FaEyeSlash /> : <FaEye />}
         </div>
       )}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import Login from "./containers/Login/Login";
+import Login from "./containers/Login/Login";
 // import ProductsTable from "./containers/ProductsTable/ProductsTable";
-import ProductsPreview from "./containers/ProductsPreview/ProductsPreview";
+// import ProductsPreview from "./containers/ProductsPreview/ProductsPreview";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ProductsPreview />
+    {/* <ProductsPreview /> */}
     {/* <ProductsTable /> */}
-    {/* <Login /> */}
+    <Login />
   </React.StrictMode>
 );
